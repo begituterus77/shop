037bcb3ec92ff18ad72044b1e79f05e5e48f75ea
doIT Nusantara <support@doit.co.id>
2024-01-31 21:03:35 +0700

    delete all

--- shop/src/data/static/site-settings.ts
+++
@@ -1,9 +0,0 @@
-import lightModeLogo from '@/assets/images/logo-black.svg';
-import darkModeLogo from '@/assets/images/logo-white.svg';
-
-export const siteSettings = {
-  lightLogo: lightModeLogo,
-  darkLogo: darkModeLogo,
-  width: 128,
-  height: 40,
-};
